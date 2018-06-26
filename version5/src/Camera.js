fb27a5b22145416948a0b539ae607c8c63574afc
EiseiKashi <eisei.kashi@sakuracode.com>
2018-06-26 16:45:24 -0300

    Code polishing

--- version5/src/Camera.js
+++ version5/src/Camera.js
@@ -1,40 +1,40 @@
 function Shinkansen (){
-	var _self = this;
-	var PI2/*Number*/     = Math.PI * 2;
+	var _self			= this;
+	var PI2				= Math.PI * 2;
 	
-	var _itemsList/*:Array*/     = new Array();
-	var _callbacksList/*:Array*/ = new Array();
+	var _itemsList		= new Array();
+	var _callbacksList	= new Array();
 	
-	var _radian/*Number*/       = 0;
-	var _focalLength/*Number*/  = 300;
+	var _radian			= 0;
+	var _focalLength	= 300;
 	
-	var _offsetX/*Number*/      = 0;
-	var _offsetY/*Number*/      = 0;
+	var _offsetX		= 0;
+	var _offsetY		= 0;
 	
-	var _cameraX/*Number*/      = 0;
-	var _cameraY/*Number*/      = 0;
-	var _cameraZ/*Number*/      = 0;
+	var _cameraX		= 0;
+	var _cameraY		= 0;
+	var _cameraZ		= 0;
 	
-	var _cameraX1/*Number*/     = -1;
-	var _cameraX2/*Number*/     = 1;
-	var _cameraY1/*Number*/     = 0;
-	var _cameraY2/*Number*/     = 0;
+	var _cameraX1		= -1;
+	var _cameraX2		= 1;
+	var _cameraY1		= 0;
+	var _cameraY2		= 0;
 	
-	var _viewPortX/*Number*/;
-	var _viewPortY/*Number*/;
+	var _viewPortX;
+	var _viewPortY;
 	
-	var _visionRadian/*Number*/;
-	var _pointLength/*:Point*/;
-	var _angle/*Number*/;
-	var _slope/*Number*/;
-	var _interceptorY/*Number*/;
+	var _visionRadian;
+	var _pointLength;
+	var _angle;
+	var _slope;
+	var _interceptorY;
 	
 	//----------------------------------------------
 	// INTERFACE
 	//----------------------------------------------
 	
-	this.getViewPortX = function ()/*Number*/ {return _viewPortX;}
-	this.setViewPortX = function (value/*Number*/)/*:void*/ {
+	this.getViewPortX = function () {return _viewPortX;}
+	this.setViewPortX = function (value) {
 		if(!isNaN(value)){
 			_viewPortX = value;
 			_self.render();
@@ -42,8 +42,8 @@ function Shinkansen (){
 		return _viewPortX;
 	}
 	
-	this.getViewPortY = function ()/*Number*/ {return _viewPortY;}
-	this.setViewPortY = function (value/*Number*/)/*:void*/ {
+	this.getViewPortY = function () {return _viewPortY;}
+	this.setViewPortY = function (value) {
 		if(!isNaN(value)){
 			_viewPortY = value;
 			_self.render();
@@ -51,28 +51,28 @@ function Shinkansen (){
 		return _viewPortY;
 	}
 	
-	this.addCallback = function (callback/*:Function*/)/*:void*/ {
+	this.addCallback = function (callback) {
 		if (_callbacksList.indexOf(callback) < 0) {
 			_callbacksList.push(callback);
 		}
 	}
 	
-	this.removeCallBack = function(callback/*:Function*/)/*:void*/ {
-		var index/*:int*/ = _callbacksList.indexOf(callback);
+	this.removeCallBack = function(callback) {
+		var index = _callbacksList.indexOf(callback);
 		if (index < 0) {
 			return;
 		}
 		_callbacksList.splice(index, 1);
 	}
 	
-	this.addItem = function(item/*:Sprite3D*/)/*:void*/ {
+	this.addItem = function(item) {
 		_itemsList.push(item);
 		_self.render();
 	}
 	
-	this.removeItem = function (item/*:Sprite3D*/)/*:void*/ {
+	this.removeItem = function (item) {
 		_itemsList.push(item);
-		var index/*:int*/ = _itemsList.indexOf(item);
+		var index = _itemsList.indexOf(item);
 		if (index < 0) {
 			return;
 		}
@@ -82,8 +82,8 @@ function Shinkansen (){
 	
 	//----------------------------------------------
 	// Desplazamiento de camera
-	this.getOffsetY = function ()/*Number*/ {return _offsetY;}
-	this.setOffsetY = function (value/*Number*/)/*:void*/ {
+	this.getOffsetY = function () {return _offsetY;}
+	this.setOffsetY = function (value) {
 		if(!isNaN(value)){
 			_offsetY = value;
 			_self.render();
@@ -91,8 +91,8 @@ function Shinkansen (){
 		return _offsetY;
 	}
 	
-	this.getOffsetX = function ()/*Number*/ {return _offsetX;}
-	this.setOffsetX = function (value/*Number*/)/*:void*/ {
+	this.getOffsetX = function () {return _offsetX;}
+	this.setOffsetX = function (value) {
 		if(!isNaN(value)){
 			_offsetX = value;
 			_self.render();
@@ -100,8 +100,8 @@ function Shinkansen (){
 		return _offsetX;
 	}
 	
-	this.getCameraX = function()/*:Number*/{return _cameraX;}
-	this.setCameraX = function(value/*Number*/)/*:void*/ {
+	this.getCameraX = function(){return _cameraX;}
+	this.setCameraX = function(value) {
 		if(!isNaN(value)){
 			_cameraX = value;
 			_self.render();
@@ -109,8 +109,8 @@ function Shinkansen (){
 		return _cameraX;
 	}
 	
-	this.getCameraY = function()/*:Number*/{return _cameraY;}
-	this.setCameraY = function(value/*Number*/)/*:void*/ {
+	this.getCameraY = function(){return _cameraY;}
+	this.setCameraY = function(value) {
 		if(!isNaN(value)){
 			_cameraY = value;
 			_self.render();
@@ -118,8 +118,8 @@ function Shinkansen (){
 		return _cameraY;
 	}
 	
-	this.getCameraZ = function()/*Number*/ {return _cameraZ;}
-	this.setCameraZ = function(value/*Number*/)/*:void*/ {
+	this.getCameraZ = function() {return _cameraZ;}
+	this.setCameraZ = function(value) {
 		if(!isNaN(value)){
 			_cameraZ = value
 			_self.render();
@@ -127,8 +127,8 @@ function Shinkansen (){
 		return _cameraZ;
 	}
 	
-	this.getAngle = function()/*Number*/ {return _angle;}
-	this.setAngle = function(value/*Number*/)/*:void*/ {
+	this.getAngle = function() {return _angle;}
+	this.setAngle = function(value) {
 		if(!isNaN(value)){
 			_self.setRadian(getRadianFromAngle(value));
 			_angle = value; 
@@ -137,8 +137,8 @@ function Shinkansen (){
 		return _angle;
 	}
 	
-	this.getRadian = function()/*Number*/ {return _radian;}
-	this.setRadian = function(value/*Number*/)/*:void*/ {
+	this.getRadian = function() {return _radian;}
+	this.setRadian = function(value) {
 		if(!isNaN(value)){
 			_radian = value;
 			_angle  = getAngleFromRadian(_radian);
@@ -147,8 +147,8 @@ function Shinkansen (){
 		return _radian;
 	}
 	
-	this.getFocalLength = function()/*Number*/ {return _focalLength;}
-	this.setFocalLength = function(value/*Number*/)/*:void*/ {
+	this.getFocalLength = function() {return _focalLength;}
+	this.setFocalLength = function(value) {
 		if(!isNaN(value)){
 			_focalLength = value;
 			_self.render();
@@ -156,9 +156,8 @@ function Shinkansen (){
 		return _focalLength;
 	}
 	
-	// _self.render!!!
-	this.render = function()/*:void*/ {
-		var length/*:uint*/ = _itemsList.length;
+	this.render = function() {
+		var length = _itemsList.length;
 		if(0 == length){
 			// Early return
 			return;
@@ -166,26 +165,25 @@ function Shinkansen (){
 		
 		updateProperties();
 		
-		var dx/*Number*/;
-		var dy/*Number*/;
-		var z/*Number*/;
-		var item/*:Sprite3D*/;
-		var itemX/*:Number*/;
-		var itemY/*:Number*/;
-		var scaleFactor/*Number*/;
-		var name/*:String*/
-		var viewName/*:String*/;
-		var isInRange/*:Boolean*/
-		var index/*:uint*/  = 0;
+		var dx;
+		var dy;
+		var z;
+		var item;
+		var itemX;
+		var itemY;
+		var scaleFactor;
+		var name;
+		var viewName;
+		var isInRange
+		var index  = 0;
 		
 		while (index < length) {
-			item                    = _itemsList[index];
-			itemX                   = item.getX()
-			itemY                   = item.getZ()
+			item	= _itemsList[index];
+			itemX	= item.getX()
+			itemY	= item.getZ()
 			
- 			var intersect/*:Point*/ =  getPointIntersection(itemX, itemY);
-			
-			isInRange = checkIsInRange(itemX, itemY);
+ 			var intersect	=  getPointIntersection(itemX, itemY);
+			isInRange		= checkIsInRange(itemX, itemY);
 			
 			if (!isInRange) {
 				item.setVisible(false);
@@ -197,18 +195,15 @@ function Shinkansen (){
 			dx = itemX - _cameraX;
 			dy = itemY - _cameraZ;
 			
-			var itemRadian/*Number*/ = Math.atan2(dy,dx) + _radian;
-			var radius/*Number*/     = getHipotenuse(dx, dy);
+			var itemRadian	= Math.atan2(dy,dx) + _radian;
+			var radius		= getHipotenuse(dx, dy);
+			z				= getPointDistance(	itemX, itemY,
+												_cameraX1, _cameraY1,
+                                            	_cameraX2, _cameraY2);
+			scaleFactor		= _focalLength / (z);
+			item.visible	= scaleFactor > 0 ;
 			
-			z              = getPointDistance(itemX, itemY, 
-			                                  _cameraX1, _cameraY1,
-                                              _cameraX2, _cameraY2);
-			
-			scaleFactor    = _focalLength / (z);
-			
-			item.visible   = scaleFactor > 0 ;
-			
-			var renderedX = (Math.cos(itemRadian) * radius * scaleFactor) + _offsetX;
+			var renderedX	= (Math.cos(itemRadian) * radius * scaleFactor) + _offsetX;
 			var renderedY = ((item.getY() - _cameraY) * scaleFactor) + _offsetY;
 			var renderedZ = z;
 			var scale     = scaleFactor;
@@ -230,10 +225,10 @@ function Shinkansen (){
 		});
 		
 		index = 0;
-		var callback/*:Function*/;
-		var indexCallback/*:uint*/ = 0;
-		var callbacksLenght/*:uint*/ = _callbacksList.length;
-		var lastIndex/*:int*/ = length-1;
+		var callback;
+		var indexCallback	= 0;
+		var callbacksLenght	= _callbacksList.length;
+		var lastIndex		= length-1;
 		while (index < length) {
 			item = _itemsList[index];
 			indexCallback = 0;
@@ -256,72 +251,68 @@ function Shinkansen (){
 	// Helpers
 	//----------------------------------------------
 	
-	var getHipotenuse  = function(dx/*Number*/, dy/*Number*/)/*Number*/ {
-		var hypotenuse/*Number*/ =  Math.sqrt(dx * dx + dy * dy);
+	var getHipotenuse  = function(dx, dy) {
+		var hypotenuse =  Math.sqrt(dx * dx + dy * dy);
 		return hypotenuse;
 	}
 	
-	var getRadianFromAngle  = function(degree/*Number*/)/*Number*/{
+	var getRadianFromAngle  = function(degree){
 		return degree * (PI2/360);
 	}
 	
-	var getAngleFromRadian  = function(radian/*Number*/)/*Number*/ {
+	var getAngleFromRadian  = function(radian) {
 		return radian * (360/PI2);
 	}
 	
-	var updateProperties  = function()/*:void*/ {
-		var halfViewPort/*Number*/ = _viewPortX / 2;
-		var visionLength/*Number*/ = getHipotenuse(halfViewPort, _focalLength);
+	var updateProperties  = function() {
+		var halfViewPort	= _viewPortX / 2;
+		var visionLength	= getHipotenuse(halfViewPort, _focalLength);
+		var leftRadian		= Math.atan2(0, halfViewPort) + _radian;
+		var rigthRadian		= Math.atan2(0, -halfViewPort)+ _radian;
 		
-		var leftRadian/*Number*/   = Math.atan2(0, halfViewPort) + _radian;
-		var rigthRadian/*Number*/  = Math.atan2(0, -halfViewPort)+ _radian;
+		_pointLength		= getRotatedPoint(0, _focalLength,  _radian,     _cameraX, _cameraZ);
+		var leftPoint		= getRotatedPoint(visionLength, 0,  leftRadian,  _cameraX, _cameraZ);
+		var rigthPoint		= getRotatedPoint(visionLength, 0,  rigthRadian, _cameraX, _cameraZ);
+		_visionRadian		= getVectorsRadian(leftPoint.x, leftPoint.y, _pointLength.x, _pointLength.y);
 		
-		_pointLength            = getRotatedPoint(0, _focalLength,  _radian,     _cameraX, _cameraZ);
-		var leftPoint/*:Point*/     = getRotatedPoint(visionLength, 0,  leftRadian,  _cameraX, _cameraZ);
-		var rigthPoint/*:Point*/    = getRotatedPoint(visionLength, 0,  rigthRadian, _cameraX, _cameraZ);
+		_cameraX1 			= leftPoint.x; 
+		_cameraY1 			= leftPoint.y;
+		_cameraX2 			= rigthPoint.x;
+		_cameraY2 			= rigthPoint.y;
 		
-		_visionRadian           = getVectorsRadian(leftPoint.x, leftPoint.y, _pointLength.x, _pointLength.y);
-		
-		_cameraX1 = leftPoint.x; 
-		_cameraY1 = leftPoint.y;
-		_cameraX2 = rigthPoint.x;
-		_cameraY2 = rigthPoint.y;
-		
-		_slope = (_cameraY2 - _cameraY1) / (_cameraX2 - _cameraX1);
-		
-		_interceptorY  = _cameraY1 - _slope * _cameraX1;
+		_slope				= (_cameraY2 - _cameraY1) / (_cameraX2 - _cameraX1);
+		_interceptorY		= _cameraY1 - _slope * _cameraX1;
 	}
 	
-	var getRotatedPoint  = function(x/*Number*/, y/*Number*/, radian/*Number*/, originX/*Number=0*/, originY/*Number=0*/)/*:Point*/ {
-		originX = getDefaultValue(originX, 0);
-		originY = getDefaultValue(originY, 0);
+	var getRotatedPoint  = function(x, y, radian, originX, originY) {
+		originX		= getDefaultValue(originX, 0);
+		originY		= getDefaultValue(originY, 0);
 		
-		var finalX/*Number*/ = x * Math.cos(radian) - y * Math.sin(radian) + originX;
-		var finalY/*Number*/ = x * Math.sin(radian) + y * Math.cos(radian) + originY;
-		var point/*:Point*/ = new Point(finalX, finalY);
+		var finalX	= x * Math.cos(radian) - y * Math.sin(radian) + originX;
+		var finalY	= x * Math.sin(radian) + y * Math.cos(radian) + originY;
+		var point	= new Point(finalX, finalY);
 		return point;
 	}
 	
-	var checkIsInRange  = function(x/*Number*/, y/*Number*/)/*:Boolean*/ {
-		var radian/*Number*/ = getVectorsRadian(_pointLength.x, _pointLength.y, x, y);
+	var checkIsInRange  = function(x, y) {
+		var radian = getVectorsRadian(_pointLength.x, _pointLength.y, x, y);
 		return _visionRadian > radian;
 	}
 	
-	var getVectorsRadian  = function(point1X/*Number*/, point1Y/*Number*/,
-									  point2X/*Number*/, point2Y/*Number*/ )/*Number*/ {
-		point1X -= _cameraX;
-		point1Y -= _cameraZ;
+	var getVectorsRadian  = function(point1X, point1Y, point2X, point2Y ) {
+		point1X		-= _cameraX;
+		point1Y		-= _cameraZ;
 		
-		point2X -= _cameraX;
-		point2Y -= _cameraZ;
+		point2X		-= _cameraX;
+		point2Y		-= _cameraZ;
 		
-		var scalar/*Number*/ =  point1X * point2X + point1Y * point2Y;
-		var mod1/*Number*/   = getHipotenuse(point1X, point1Y);
-		var mod2/*Number*/   = getHipotenuse(point2X, point2Y);
-		var mods/*Number*/   = (mod1 * mod2); 
+		var scalar	=  point1X * point2X + point1Y * point2Y;
+		var mod1	= getHipotenuse(point1X, point1Y);
+		var mod2	= getHipotenuse(point2X, point2Y);
+		var mods	= (mod1 * mod2); 
 		
-		var radianToCalculate/*:Number*/ = mods == 0 ? 0 : scalar / mods;
-		var radian/*:Number*/= Math.acos(radianToCalculate);
+		var radianToCalculate = mods == 0 ? 0 : scalar / mods;
+		var radian= Math.acos(radianToCalculate);
 		if(isNaN(radian)){
 			return;
 		}
@@ -332,10 +323,10 @@ function Shinkansen (){
 		return radian;
 	}
 	
-	var getPointDistance  = function(itemX/*Number*/, itemY/*Number*/, 
-									  x1/*Number*/, y1/*Number*/, x2/*Number*/, y2/*Number*/)/*Number*/ {
-		var denominator/*Number*/ = Math.abs((y2 - y1) * itemX - (x2 - x1) * itemY + x2 * y1 - y2 * x1);
-		var divisor/*Number*/     = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 -x1, 2));
+	var getPointDistance  = function(itemX, itemY, 
+									  x1, y1, x2, y2) {
+		var denominator = Math.abs((y2 - y1) * itemX - (x2 - x1) * itemY + x2 * y1 - y2 * x1);
+		var divisor     = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 -x1, 2));
 		return denominator / divisor;
 		
 		// d = 	|A·Mx + B·My + C|
@@ -343,114 +334,110 @@ function Shinkansen (){
 		//https://www.youtube.com/watch?v=bfZ57ESvFok
 	}
 	
-	var getPointIntersection  = function(itemX/*Number*/, itemY/*Number*/)/*:Point*/ {
-		var m2/*Number*/ = -(1 / _slope);
-		var b2/*Number*/  = itemY - m2 * itemX;
-		var x2/*Number*/  = (b2-_interceptorY)/(_slope-m2);
-		var y2/*Number*/  = (m2 * x2 + b2);
+	var getPointIntersection  = function(itemX, itemY) {
+		var m2 = -(1 / _slope);
+		var b2  = itemY - m2 * itemX;
+		var x2  = (b2-_interceptorY)/(_slope-m2);
+		var y2  = (m2 * x2 + b2);
 		return new Point(x2, y2);
 	}
 	
-	var Point  = function(x/*:Number*/, y/*:Number*/) {
+	var Point  = function(x, y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	var getDefaultValue = function(value, defaultValue)/*:void*/ {
+	var getDefaultValue = function(value, defaultValue) {
 		var finalValue = typeof(value) === "undefined" ? defaultValue : value;
 		return finalValue;
 	}
 }
 
 
-window.Sprite3D = function (view/*:MovieClip=null*/, 
-						    x/*:Number=0*/, 
-							y/*:Number=0*/, 
-							z/*:Number=0*/) {
-
-	var _view = null != view ? view : null;
-	var _x    = isNaN(x) ? 0 : x;
-	var _y    = isNaN(y) ? 0 : y;
-	var _z    = isNaN(z) ? 0 : z;
+window.Sprite3D = function (view, x, y, z) {
+	var _view		= null != view ? view : null;
+	var _x			= isNaN(x) ? 0 : x;
+	var _y			= isNaN(y) ? 0 : y;
+	var _z			= isNaN(z) ? 0 : z;
 	
-	var _renderedX/*:Number*/ = 0;
-	var _renderedY/*:Number*/ = 0;
-	var _renderedZ/*:Number*/ = 0;
+	var _renderedX	= 0;
+	var _renderedY	= 0;
+	var _renderedZ	= 0;
 	
-	var _visible/*:Boolean*/  = true;
+	var _visible	= true;
 	
-	var _scale/*:Number*/     = 0;
+	var _scale		= 0;
 	
-	this.getId = function()/*:String*/ {
+	this.getId = function() {
 		return _view != null ? _view.name : null;
 	}
 	
-	this.getView = function()/*:MovieClip*/ {return _view;}
-	this.setView = function(value/*:MovieClip*/)/*:void*/ {
+	this.getView = function() {return _view;}
+	this.setView = function(value) {
 		if (null != value) {
 			_view = value;
 		}
 		return _view;
 	}
 	
-	this.getX = function()/*:Number*/ {return _x;}
-	this.setX = function(value/*:Number*/)/*:void*/ {
+	this.getX = function() {return _x;}
+	this.setX = function(value) {
 		if (!isNaN(value)) {
 			_x = value;
 		}
 		return _x;
 	}
 	
-	this.getY = function()/*:Number*/ {return _y;}
-	this.setY = function(value/*:Number*/)/*:void*/ {
+	this.getY = function() {return _y;}
+	this.setY = function(value) {
 		if (!isNaN(value)) {
 			_y = value;
 		}
 		return _y;
 	}
 	
-	this.getZ = function()/*:Number*/ {return _z;}
-	this.setZ = function(value/*:Number*/)/*:void*/ {
+	this.getZ = function() {return _z;}
+	this.setZ = function(value) {
 		if (!isNaN(value)) {
 			_z = value;
 		}
 		return _z;
 	}
 	
-	this.getRenderedX = function()/*:Number*/ {return _renderedX;}
-	this.setRenderedX = function(value/*:Number*/)/*:void*/ {
+	this.getRenderedX = function() {return _renderedX;}
+	this.setRenderedX = function(value) {
 		if (!isNaN(value)) {
 			_renderedX = value;
 		}
 		return _renderedX;
 	}
 	
-	this.getRenderedY = function()/*:Number*/ {return _renderedY;}
-	this.setRenderedY = function(value/*:Number*/)/*:void*/ {
+	this.getRenderedY = function() {return _renderedY;}
+	this.setRenderedY = function(value) {
 		if (!isNaN(value)) {
 			_renderedY = value;
 		}
 		return _renderedY;
 	}
 	
-	this.getRenderedZ = function()/*:Number*/ {return _renderedZ;}
-	this.setRenderedZ = function(value/*:Number*/)/*:void*/ {
+	this.getRenderedZ = function() {return _renderedZ;}
+	this.setRenderedZ = function(value) {
 		if (!isNaN(value)) {
 			_renderedZ = value;
 		}
 		return _renderedZ;
 	}
 	
-	this.getScale = function()/*:Number*/ {return _scale;}
-	this.setScale = function(value/*:Number*/)/*:void*/ {
+	this.getScale = function() {return _scale;}
+	this.setScale = function(value) {
 		if(!isNaN(value)){
 			_scale = value;
 		}
 		return _scale;
 	}
 	
-	this.getVisible = function()/*:Boolean*/ {return _visible;}
-	this.setVisible = function(value/*:Boolean*/)/*:void*/ {
+	this.getVisible = function() {return _visible;}
+	this.setVisible = function(value) {
 		_visible = value;
 	}
 }
