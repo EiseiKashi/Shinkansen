46b4a7f4e150d680d8e86b96e90d90420251389f
EiseiKashi <eisei.kashi@sakuracode.com>
2018-06-30 10:53:18 -0300

    Canvas representation

--- version5/js/Main.js
+++ version5/js/Main.js
@@ -1,9 +1,15 @@
+var canvas 		  = document.getElementById("map");
+	canvas.width  = 600;
+	canvas.height = 600;
+
+var canvate = new Canvate(canvas);
+
 var shinkansen;
 	shinkansen = new Shinkansen();
-	shinkansen.setViewPortX (800);
-	shinkansen.setViewPortY (600);
-	shinkansen.setOffsetX   (800 / 2);
-	shinkansen.setOffsetY   (600 / 2);
+	shinkansen.setViewPortX (200);
+	shinkansen.setViewPortY (200);
+	shinkansen.setOffsetX   (200 / 2);
+	shinkansen.setOffsetY   (200 / 2);
 		
 var node;
 var item;
@@ -19,38 +25,43 @@ var indexX			= 1;
 var index			= 0;
 var x;
 var y;
-		while (rowIndex < totalRows) {
-			columnIndex = 0;
-			while(columnIndex < totalColumns) {
-				node	= document.getElementById("imagen" + index);
-				x		= columnIndex*_size;
-				y		= rowIndex*_size;
-				item	= shinkansen.addNew(node, x, 0, y);
-				index++
-				columnIndex ++;
-			}
-			rowIndex++;
-		}
-		
-		shinkansen.cameraX = 570;
-		shinkansen.cameraY = 0;
-		shinkansen.cameraZ = 15;
-		
-		var camera = 570;
-		
-		shinkansen.addCallback(onRender);
-		shinkansen.rotation = 90;
-		
-		setInterval(function(){
-			shinkansen.cameraX = camera;
-			camera -= 1;
-		}, 30);
-	
-	function onRender(view, index, x, y, z, scale, visible) {
-		view.style.display = visible ? "inline" : "none";
-		view.style.left    = x + "px";
-		view.style.top     = y + "px";
-		view.style.width   = (scale*99) + "px";
-		view.style.height  = (scale*99) + "px";
-		view.style.zIndex  = index;
+while (rowIndex < totalRows) {
+	columnIndex = 0;
+	while(columnIndex < totalColumns) {
+		node	= document.getElementById("imagen" + index);
+		x		= columnIndex*_size;
+		y		= rowIndex*_size;
+		item	= shinkansen.addNew(node, x, 0, y);
+		var rate = .5;
+		var img = canvate.addNew(node);
+			img.setScale(rate/5, rate/5);
+			img.x = x*rate;
+			img.y = y*rate;
+		index++
+		columnIndex ++;
 	}
+	rowIndex++;
+}
+
+shinkansen.cameraX = 570;
+shinkansen.cameraY = 0;
+shinkansen.cameraZ = 15;
+
+var camera = 570;
+
+shinkansen.addCallback(onRender);
+shinkansen.rotation = 90;
+
+setInterval(function(){
+	shinkansen.cameraX = camera;
+	camera -= 1;
+}, 30);
+
+function onRender(view, index, x, y, z, scale, visible) {
+	view.style.display = visible ? "inline" : "none";
+	view.style.left    = x + "px";
+	view.style.top     = y + "px";
+	view.style.width   = (scale*99) + "px";
+	view.style.height  = (scale*99) + "px";
+	view.style.zIndex  = index;
+}
